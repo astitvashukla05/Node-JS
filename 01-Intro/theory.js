16bf048d238350089c52740f2da3cfbc1409cf44
Astitva Shukla <astitvashukla1605@gmail.com>
2025-09-27 10:29:40 +0530

    Node Js setup and some intro

---
+++ 01-Intro/theory.js
@@ -0,0 +1,26 @@
+const fs = require('fs'); // Take ours file system to manipulate it
+fs, fs.writeFileSync('node.txt', 'Hello From node.js'); // Creates a file and text in it
+/*
+ What is Node JS ?
+ As we know that js is a client site language and only runs on browser (console) , but node JS provides a run time env to,
+ js so that it can run at server side  as well.
+ Node JS uses v8 the engine developed by google
+*/
+/*
+ Some uses of  Node js
+ 1.)Used for creating servers : Run the server and listen to incoming requests
+ 2.)Bussiness logics : Connect databases , validate data,Handle Requests
+ 3.)Return Responses back in the form of html ,JSON
+ */
+/*
+ Some protocols are followed such as HTTP or HTTPS
+ */
+/*
+ Core Modules for node
+ http
+ https
+ path 
+ os 
+ fs 
+
+*/
